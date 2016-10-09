d5acd551496ad17659b791c7109031e26c1b019d
Joshua Igor <worzyy@gmail.com>
2016-10-09 15:24:59 -0300

    Update comprovante.blade.php

--- ProtocoloAtivo/resources/views/comprovante.blade.php
+++ ProtocoloAtivo/resources/views/comprovante.blade.php
@@ -1,38 +1,54 @@
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Comprovante</title>
-	<link href="{{URL::asset('lib/comprovante.css')}}" rel="stylesheet">
-	<script language="JavaScript"> 
-        window.open("comprovante.pdf");
-	</script>
-</head>
-<body>
-<section>
-	<div class="box">
-	<p><img src="{{ URL::asset('img/logo2x2.png')}}" class="img-responsive" height="85" width="125"></img><b>SISTEMA DE PROTOCOLO PRO-ATIVO CONTABILIDADE</b></p>
-
-		<p>NÚMERO DO PROTOCOLO: {{$protocolo->id}}</p>
-
-		<p>DATA DE EMISSÃO: {{$protocolo->created_at}}</p>
-
-		<p>SETOR DE EMISSÃO: {{$protocolo->snome}}</p>
-
-		<p>RESPONSÁVEL DO SETOR: </p>
-
-		<p>DESTINO: {{$protocolo->dnome}}</p>
-		<hr>
-
-			<p>TIPO DE DOCUMENTO: {{$protocolo->tdocumento}}</p><br>
-		<hr>
-		<p>DESCRIÇÃO ANALÍTICA DO DOCUMENTO: {{$protocolo->inf_adicionais}}</p><br>
-		<hr>
-			<p>NOME DO RECEBEDOR: </p><br>
-			<p>ASSINATURA DO RECEBEDOR: </p><br>
-
-			<p>DATA E HORA DE RECEBIMENTO: </p><br>
-
-	</div>
-</section>
-</body>
-<html>
+<!DOCTYPE html>
+<html class="html" lang="en-GB">
+ <head>  
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="css/comprovante.css" id="pagesheet"/>
+  </head>
+ <body>
+  <div class="clearfix" id="page">
+   <div class="position_content" id="page_position_content">
+    <div class="clearfix colelem" id="pu106-4">
+     <div class="clearfix grpelem" id="u106-4">
+      <p>SISTEMA DE PROTOCOLO ÚNICO - PROATIVO CONTABILIDADE</p>
+     </div>
+     <div class="clip_frame grpelem" id="u124">
+      <img class="block" id="u124_img" src="images/logo.png" alt="" width="54" height="39"/>
+     </div>
+    </div>
+    <div class="clearfix colelem" id="pu108-8">
+     <div class="clearfix grpelem" id="u108-8">
+      <p id="u108-2">NÚMERO DO PROTOCOLO:</p>
+      <p id="u108-4">SETOR DE EMISSÃO:</p>
+      <p id="u108-6">DESTINO:</p>
+     </div>
+     <div class="clearfix grpelem" id="u109-4">
+      <p>DATA EMISSÃO:</p>
+     </div>
+     <div class="clearfix grpelem" id="u110-4">
+      <p>RESPONSÁVEL DO SETOR:</p>
+     </div>
+    </div>
+    <div class="colelem" id="u111"></div>
+    <div class="clearfix colelem" id="u113-4">
+     <p id="u113-2">TIPO DE DOCUMENTO:</p>
+    </div>
+    <div class="clearfix colelem" id="u115-4">
+     <p id="u115-2">DESCRIÇÃO ANALÍTICA DO DOCUMENTO:</p>
+    </div>
+    <div class="colelem" id="u116"></div>
+    <div class="clearfix colelem" id="pu118-8">
+     <div class="clearfix grpelem" id="u118-8">
+      <p id="u118-2">NOME DO RECEBEDOR:______________________________________________________________________________________________</p>
+      <p id="u118-4">ASSINATURA DO RECEBEDOR:________________________________________________________________________________________</p>
+      <p id="u118-6">DATA DE RECEBIMENTO:________________________________</p>
+     </div>
+     <div class="clearfix grpelem" id="u122-4">
+      <p>HORA DE RECEBIMENTO:______________________________________</p>
+     </div>
+    </div>
+    <div class="verticalspacer"></div>
+   </div>
+  </div>
+  </body>
+</html>
